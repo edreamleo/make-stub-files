1656610a04337860929ca6dfd0b02651663e8aa5
Edward K. Ream <edreamleo@gmail.com>
2016-02-14 17:54:19 -0600

    updated test/test_reduce_types.py. This is the acid test

--- test/test_reduce_types.py
+++ test/test_reduce_types.py
@@ -6,25 +6,32 @@ class test_reduce_types (unittest.TestCase):
         a, c, f, i, l, n = ('Any', 'complex', 'float', 'int', 'long', 'number')
         none = 'None'
         x = 'xyzzy'
+        y = 'pdq'
         table = (
-            ([i,i],     i),
-            ([i],       i),
-            ([f, i],    f),
-            ([c, i],    c),
-            ([l, a],    'Union[Any, long]'),
+            ([i,i],         i),
+            ([i],           i),
+            ([f, i],        f),
+            ([c, i],        c),
+            ([l, a],        'Union[Any, long]'),
             # Handle None
             ([None],        none),
             ([None, None],  none),
-            ([None, a, c],  'Union[Any, complex]'),
-            # Handle unknown types.
+            ([None, a, c],  'Optional[Union[Any, complex]]'),
+            # Handle unknown types, and special cases
             ([i, x],        'Union[Any, int]'),
-            # Collection merging...
-            (['Dict[int, str]', 'Dict[Any, str]'], 'Union[Dict[Any, str], Dict[int, str]]'),
-            (['List[int, str]', 'List[Any, str]'], 'Union[List[Any, str], List[int, str]]'),
-            (['Union[int, str]', 'Union[Any, str]'], 'Union[Any, int, str]'),
-            (['Union[int, str]', 'int', 'Union[Any, str]'], 'Union[Any, int, str]'),
+            ([None, x],     'Optional[Any]'),
+            ([none, x],     'Optional[Any]'),
+            (['', x],       'Optional[Any]'),
+            ([none, x, c],  'Optional[Union[Any, complex]]'),
+            ([x, y],        'Any'),
+            # Collection merging.  More could be done...
+            (['Dict[int, str]', 'Dict[Any, str]'],          'Union[Dict[Any, str], Dict[int, str]]'),
+            (['List[int, str]', 'List[Any, str]'],          'Union[List[Any, str], List[int, str]]'),
+            (['Union[int, str]', 'Union[Any, str]'],        'Union[Union[Any, str], Union[int, str]]'),
+            (['Union[int, str]', 'int', 'Union[Any, str]'], 'Union[Union[Any, str], Union[int, str], int]'),
+            (['Tuple[xyz, pdq]'],                           'Tuple[Any, Any]'),
         )
         for aList, expected in table:
             got = ReduceTypes(aList).reduce_types()
-            assert expected == got, (aList, 'expected:', expected, 'got', got)
+            assert expected == got, '\naList:    %s\nexpected: %s\ngot:      %s' % (aList, expected, got)
 
