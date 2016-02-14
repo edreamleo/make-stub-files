430f1555c290c082e4bdf9d69e3fa9e33cdbf7a6
Edward K. Ream <edreamleo@gmail.com>
2016-02-14 07:25:14 -0600

    updated external version of recently-changed unit test

--- test/test_reduce_types.py
+++ test/test_reduce_types.py
@@ -15,6 +15,11 @@ class test_reduce_types (unittest.TestCase):
             ([None],        none),
             ([None, None],  none),
             ([None, a, c],  'Union[Any, complex]'),
+            # Tuple merging
+            (['Dict[int, str]', 'Dict[Any, str]'], 'Dict[Any, int, str]'),
+            (['List[int, str]', 'List[Any, str]'], 'List[Any, int, str]'),
+            (['Union[int, str]', 'Union[Any, str]'], 'Union[Any, int, str]'),
+            (['Union[int, str]', 'int', 'Union[Any, str]'], 'Union[Union[Any, int, str], int]'),
         )
         for aList, expected in table:
             got = ReduceTypes(aList).reduce_types()
